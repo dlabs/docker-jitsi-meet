aaf6e3e76460e98061c83cd7342743a655c07230
Urban Soban <urban.soban@dlabs.si>
2021-01-29 09:34:04 +0100

    Fix for finalize script.

--- jibri/rootfs/defaults/finalize.sh
+++ jibri/rootfs/defaults/finalize.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
-
 AWS_ACCESS_KEY=
 AWS_SECRET_KEY=
 AWS_DEFAULT_REGION=eu-west-1
+
+# Should be replaced by env var
 S3_BUCKET_NAME=jibri-video-dev
 
+# /config/recordings/<jibri-generated-dirname>
 RECORDINGS_DIR=$1
 
-echo $RECORDINGS_DIR
-echo "Uploading to S3"
+# extract <jibri-generated-dirname> and use for S3 key (dir)
+RECORDING_KEY=$(echo "$RECORDINGS_DIR" | sed 's|.*/||')
 
-RECORDINGS=`ls $RECORDINGS_DIR/`
-RECORDINGS=`basename $RECORDINGS`
-FOLDER_NAME=$(echo $RECORDINGS | tr "_" "\n")
-
-for name in $FOLDER_NAME
-do
-    FOLDER_NAME=$name
-break
-done
+echo "[FINALIZE]: WILL MOVE FILES FROM ${RECORDINGS_DIR} TO s3://${S3_BUCKET_NAME}/${RECORDING_KEY}"
 
 upload(){
     echo "Uploading..."
     aws configure set aws_access_key_id $AWS_ACCESS_KEY
     aws configure set aws_secret_access_key $AWS_SECRET_KEY
     aws configure set default.region $AWS_DEFAULT_REGION
-    aws s3 sync $RECORDINGS_DIR s3://$S3_BUCKET_NAME/$FOLDER_NAME
+    aws s3 mv $RECORDINGS_DIR s3://$S3_BUCKET_NAME/$RECORDING_KEY --recursive
 }
 
 upload
